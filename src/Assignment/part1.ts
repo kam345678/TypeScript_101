9b5345e069e9be9651f1b34f0a90084ff9a2a5a0
kam345678 <tk.kam23132@gmail.com>
2025-08-25 12:23:16 +0700

    Merge part1.ts  files and part2.ts files as part1-and-part2.ts name

--- src/Assignment/part1.ts
+++ src/Assignment/part1.ts
@@ -1,35 +1,35 @@
 
-// ประกาศ Employee เป็น Interface 
+// // ประกาศ Employee เป็น Interface 
 
-interface Employee {
-    // ประกาศตัวแปรใน Employee มีข้อมูล: id, name, position, salary 
-    id: number 
-    name: string
-    position: string
-    salary: number
-}
-
-
-// สร้างอาเรย์ employees: Employee[ ] กำหนดข้อมูลของพนักงานอย่างน้อย 3 คน  
-const employees: Employee[] = [
-  { id: 1, name: "Alice", position: "Developer", salary: 35000 },
-  { id: 2, name: "Bob", position: "Designer", salary: 30000 },
-  { id: 3, name: "Charlie", position: "Manager", salary: 45000 },
-];
-
-// เขียนฟังก์ชัน findEmployeeByName 
-function findEmployeeByName(name: string): Employee | undefined {
-  return employees.find(emp => emp.name === name);
-}
+// interface Employee {
+//     // ประกาศตัวแปรใน Employee มีข้อมูล: id, name, position, salary 
+//     id: number 
+//     name: string
+//     position: string
+//     salary: number
+// }
 
 
-// แสดงผลข้อมูลพนักงานทั้งหมด 
-console.log("ข้อมูลพนักงานทั้งหมด:");
-employees.forEach(emp => {
-  console.log(emp);
-});
+// // สร้างอาเรย์ employees: Employee[ ] กำหนดข้อมูลของพนักงานอย่างน้อย 3 คน  
+// const employees: Employee[] = [
+//   { id: 1, name: "Alice", position: "Developer", salary: 35000 },
+//   { id: 2, name: "Bob", position: "Designer", salary: 30000 },
+//   { id: 3, name: "Charlie", position: "Manager", salary: 45000 },
+// ];
+
+// // เขียนฟังก์ชัน findEmployeeByName 
+// function findEmployeeByName(name: string): Employee | undefined {
+//   return employees.find(emp => emp.name === name);
+// }
 
 
-// แสดงผลการค้นหาพนักงาน 
-console.log("\nค้นหาพนักงานชื่อ Bob:");
-console.log(findEmployeeByName("Bob"));
+// // แสดงผลข้อมูลพนักงานทั้งหมด 
+// console.log("ข้อมูลพนักงานทั้งหมด:");
+// employees.forEach(emp => {
+//   console.log(emp);
+// });
+
+
+// // แสดงผลการค้นหาพนักงาน 
+// console.log("\nค้นหาพนักงานชื่อ Bob:");
+// console.log(findEmployeeByName("Bob"));
